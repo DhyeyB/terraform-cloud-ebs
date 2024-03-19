0728b21cbf9bf53545e97c9fb12c832032716c3c
bs-dhyey-bhattasana <dhyey.bhattasana@bombaysoftwares.com>
2024-03-19 11:33:01 +0530

    cicd updated

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
+# Terraform Variables
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
 variable "app_name" {
   description = "Application Name"
   type        = string
